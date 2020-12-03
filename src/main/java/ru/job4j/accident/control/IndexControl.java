214c43fb9f6e8726d2ab3c6ea1cea8ab9b81c096
mac <faimon96@gmail.com>
2020-12-03 14:12:46 +0700

    1. Загрузка данных в вид [#373009]

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("user", "Alexandr");
         return "index";
     }
 }
